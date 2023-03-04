94b59d5b3be84ac445e6395bc0fdeff7725a4180
rahulrauki <rahulrauki@gmail.com>
2023-03-05 00:28:11 +0530

    plot dropdowns
    Signed-off-by: rahulrauki <rahulrauki@gmail.com>

--- src/app/plot-page/plot-page.component.ts
+++ src/app/plot-page/plot-page.component.ts
@@ -24,19 +24,22 @@ export class PlotPageComponent {
     console.log(this.plotType);
   }
 
-  trace1 : TraceData = {
-    x: [1, 2, 3, 4],
-    y: [10, 15, 13, 17],
+  trace1 = {
+    // x: [1, 2, 3, 4],
+    x: [],
+    // y: [10, 15, 13, 17],
+    y: [],
     type: 'scatter'
   };
   
-  trace2 = {
+  trace2 : TraceData  = {
     x: [1, 2, 3, 4],
     y: [16, 5, 11, 9],
     type: 'scatter'
   };
   
-  data = [this.trace1, this.trace2];
+  // data = [this.trace1, this.trace2];
+  data = [this.trace1];
   
   layout = {width: 900, height: 526, title: 'A Fancy Plot'}
 
